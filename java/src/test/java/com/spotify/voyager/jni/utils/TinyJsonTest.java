a3cb27a0e0abbec7976bad7d03d493a243e6572b
Peter Sobot <psobot@spotify.com>
2023-06-02 13:48:51 -0400

    Formatting and style.

--- java/src/test/java/com/spotify/voyager/jni/utils/TinyJsonTest.java
+++ java/src/test/java/com/spotify/voyager/jni/utils/TinyJsonTest.java
@@ -1,3 +1,24 @@
+/*-
+ * -\-\-
+ * voyager
+ * --
+ * Copyright (C) 2016 - 2023 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+
 package com.spotify.voyager.jni.utils;
 
 import org.junit.Test;
@@ -12,25 +33,25 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-public class TinyJSONTest {
+public class TinyJsonTest {
     @Test
     public void testSerializeList() throws IOException {
         final ByteArrayOutputStream os = new ByteArrayOutputStream();
-        TinyJSON.writeStringList(Arrays.asList("a", "b", "c"), os);
+        TinyJson.writeStringList(Arrays.asList("a", "b", "c"), os);
         assertEquals("[\"a\",\"b\",\"c\"]", os.toString());
     }
 
     @Test
     public void testDeserializeList() {
         final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\",\"b\",\"c\"]".getBytes(UTF_8));
-        List<String> result = TinyJSON.readStringList(is);
+        List<String> result = TinyJson.readStringList(is);
         assertEquals(Arrays.asList("a", "b", "c"), result);
     }
 
     @Test
     public void testDeserializeListWithWhitespace() {
         final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\", \"b\",\n\n \"c\"]".getBytes(UTF_8));
-        List<String> result = TinyJSON.readStringList(is);
+        List<String> result = TinyJson.readStringList(is);
         assertEquals(Arrays.asList("a", "b", "c"), result);
     }
 
@@ -38,7 +59,7 @@ public class TinyJSONTest {
     public void testDeserializeNonList() {
         final ByteArrayInputStream is = new ByteArrayInputStream("{}".getBytes(UTF_8));
         try {
-            TinyJSON.readStringList(is);
+            TinyJson.readStringList(is);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
@@ -48,7 +69,7 @@ public class TinyJSONTest {
     public void testDeserializeNonStringList() {
         final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\", 1]".getBytes(UTF_8));
         try {
-            TinyJSON.readStringList(is);
+            TinyJson.readStringList(is);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
@@ -58,7 +79,7 @@ public class TinyJSONTest {
     public void testDeserializeInvalidList() {
         final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\"}".getBytes(UTF_8));
         try {
-            TinyJSON.readStringList(is);
+            TinyJson.readStringList(is);
             fail();
         } catch (IllegalArgumentException ignored) {
         }
