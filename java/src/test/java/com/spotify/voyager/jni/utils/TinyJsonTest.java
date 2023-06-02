780b3685623bd6bf45adbbaa8849602199f6b1bb
Peter Sobot <psobot@spotify.com>
2023-06-02 13:59:45 -0400

    Formatting.

--- java/src/test/java/com/spotify/voyager/jni/utils/TinyJsonTest.java
+++ java/src/test/java/com/spotify/voyager/jni/utils/TinyJsonTest.java
@@ -18,70 +18,69 @@
  * -/-/-
  */
 
-
 package com.spotify.voyager.jni.utils;
 
-import org.junit.Test;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import org.junit.Test;
 
 public class TinyJsonTest {
-    @Test
-    public void testSerializeList() throws IOException {
-        final ByteArrayOutputStream os = new ByteArrayOutputStream();
-        TinyJson.writeStringList(Arrays.asList("a", "b", "c"), os);
-        assertEquals("[\"a\",\"b\",\"c\"]", os.toString());
-    }
+  @Test
+  public void testSerializeList() throws IOException {
+    final ByteArrayOutputStream os = new ByteArrayOutputStream();
+    TinyJson.writeStringList(Arrays.asList("a", "b", "c"), os);
+    assertEquals("[\"a\",\"b\",\"c\"]", os.toString());
+  }
 
-    @Test
-    public void testDeserializeList() {
-        final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\",\"b\",\"c\"]".getBytes(UTF_8));
-        List<String> result = TinyJson.readStringList(is);
-        assertEquals(Arrays.asList("a", "b", "c"), result);
-    }
+  @Test
+  public void testDeserializeList() {
+    final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\",\"b\",\"c\"]".getBytes(UTF_8));
+    List<String> result = TinyJson.readStringList(is);
+    assertEquals(Arrays.asList("a", "b", "c"), result);
+  }
 
-    @Test
-    public void testDeserializeListWithWhitespace() {
-        final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\", \"b\",\n\n \"c\"]".getBytes(UTF_8));
-        List<String> result = TinyJson.readStringList(is);
-        assertEquals(Arrays.asList("a", "b", "c"), result);
-    }
+  @Test
+  public void testDeserializeListWithWhitespace() {
+    final ByteArrayInputStream is =
+        new ByteArrayInputStream("[\"a\", \"b\",\n\n \"c\"]".getBytes(UTF_8));
+    List<String> result = TinyJson.readStringList(is);
+    assertEquals(Arrays.asList("a", "b", "c"), result);
+  }
 
-    @Test
-    public void testDeserializeNonList() {
-        final ByteArrayInputStream is = new ByteArrayInputStream("{}".getBytes(UTF_8));
-        try {
-            TinyJson.readStringList(is);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-        }
+  @Test
+  public void testDeserializeNonList() {
+    final ByteArrayInputStream is = new ByteArrayInputStream("{}".getBytes(UTF_8));
+    try {
+      TinyJson.readStringList(is);
+      fail();
+    } catch (IllegalArgumentException ignored) {
     }
+  }
 
-    @Test
-    public void testDeserializeNonStringList() {
-        final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\", 1]".getBytes(UTF_8));
-        try {
-            TinyJson.readStringList(is);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-        }
+  @Test
+  public void testDeserializeNonStringList() {
+    final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\", 1]".getBytes(UTF_8));
+    try {
+      TinyJson.readStringList(is);
+      fail();
+    } catch (IllegalArgumentException ignored) {
     }
+  }
 
-    @Test
-    public void testDeserializeInvalidList() {
-        final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\"}".getBytes(UTF_8));
-        try {
-            TinyJson.readStringList(is);
-            fail();
-        } catch (IllegalArgumentException ignored) {
-        }
+  @Test
+  public void testDeserializeInvalidList() {
+    final ByteArrayInputStream is = new ByteArrayInputStream("[\"a\"}".getBytes(UTF_8));
+    try {
+      TinyJson.readStringList(is);
+      fail();
+    } catch (IllegalArgumentException ignored) {
     }
+  }
 }
