61761341d24a71edcd444863c38a3157ad14f17b
Peter Sobot <psobot@spotify.com>
2023-06-05 16:28:18 -0400

    Clean up StringIndex Java tests and get them passing again.

--- java/src/test/java/com/spotify/voyager/jni/StringIndexTest.java
+++ java/src/test/java/com/spotify/voyager/jni/StringIndexTest.java
@@ -21,13 +21,11 @@
 package com.spotify.voyager.jni;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.google.common.io.Resources;
 import com.spotify.voyager.jni.Index.SpaceType;
 import com.spotify.voyager.jni.Index.StorageDataType;
 import com.spotify.voyager.jni.StringIndex.QueryResults;
-import com.spotify.voyager.jni.TestUtils.ListVector;
 import com.spotify.voyager.jni.TestUtils.Vector;
 import java.io.File;
 import java.nio.file.Files;
@@ -38,6 +36,7 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FileUtils;
+import org.junit.Test;
 
 public class StringIndexTest {
   private static final Function<QueryResults, List<CustomResult>> RESULT_MAPPER =
@@ -50,136 +49,115 @@ public class StringIndexTest {
         return customResults;
       };
 
-  // @Test
-  public void itLoadsFromFileSystem() {}
+  private static final String TEMP_DIR_NAME = ".voyager-test-temp";
+  private static final String EXPECTED_INDEX_FILE_NAME = "index.hnsw";
+  private static final String EXPECTED_NAME_FILE_NAME = "names.json";
 
-  // @Test
+  @Test
   public void itFindsNeighbors() throws Exception {
-    StringIndex index = new StringIndex(SpaceType.Cosine, 80, 16, 200, 0, 50, StorageDataType.E4M3);
-    List<Vector> vectors = TestUtils.getTestVectors();
+    List<Vector> testVectors = TestUtils.getTestVectors();
+    try (final StringIndex index = new StringIndex(
+            SpaceType.Cosine, testVectors.get(0).vector.length, 20, testVectors.size(), 0, testVectors.size(), StorageDataType.E4M3)) {
+      for (Vector v : testVectors) {
+        index.addItem(v.name, v.vector);
+      }
 
-    for (Vector v : vectors) {
-      index.addItem(v.uri, v.vector);
+      List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, testVectors.size()));
+      assertThat(results)
+              .extracting(CustomResult::getName)
+              .containsExactly("my-vector-78", "my-vector-93");
     }
-
-    List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, 2));
-    assertThat(results)
-        .extracting(CustomResult::getName)
-        .containsExactly(
-            "spotify:track:0Eqm7hD828cATBLUx2fJox", "spotify:track:0Eq2AzJQNwxf5FsLYBjitC");
-    index.close();
   }
 
-  // @Test
-  public void itFindsNeighborsUsingList() throws Exception {
-    StringIndex index = new StringIndex(SpaceType.Cosine, 80, 16, 200, 0, 50, StorageDataType.E4M3);
-    List<ListVector> vectors = TestUtils.getTestVectorsUsingList();
-
-    for (ListVector v : vectors) {
-      index.addItem(v.uri, v.vector);
-    }
-
-    List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, 2));
-    assertThat(results)
-        .extracting(CustomResult::getName)
-        .containsExactly(
-            "spotify:track:0Eqm7hD828cATBLUx2fJox", "spotify:track:0Eq2AzJQNwxf5FsLYBjitC");
-    index.close();
-  }
-
-  // @Test
+  @Test
   public void itAddsItemsInBatch() throws Exception {
-    StringIndex index = new StringIndex(SpaceType.Cosine, 80, 16, 200, 0, 50, StorageDataType.E4M3);
-    Map<String, List<Float>> vectors =
-        TestUtils.getTestVectorsUsingList().stream()
-            .collect(Collectors.toMap(vec -> vec.uri, vec -> vec.vector));
+    List<Vector> testVectors = TestUtils.getTestVectors();
+    try (final StringIndex index = new StringIndex(
+            SpaceType.Cosine, testVectors.get(0).vector.length, 16, testVectors.size(), 0, testVectors.size(), StorageDataType.E4M3)) {
 
-    index.addItems(vectors);
+      Map<String, List<Float>> vectors =
+              TestUtils.getTestVectors().stream()
+                      .collect(Collectors.toMap(vec -> vec.name, vec -> convert(vec.vector)));
+      index.addItems(vectors);
 
-    List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, 2));
-    assertThat(results)
-        .extracting(CustomResult::getName)
-        .containsExactly(
-            "spotify:track:0Eqm7hD828cATBLUx2fJox", "spotify:track:0Eq2AzJQNwxf5FsLYBjitC");
-    index.close();
+      List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, testVectors.size()));
+      assertThat(results)
+              .extracting(CustomResult::getName)
+              .containsExactly("my-vector-78", "my-vector-93");
+    }
   }
 
-  // @Test
+  @Test
   public void itSavesToOutputStream() throws Exception {
-    StringIndex index = new StringIndex(SpaceType.Cosine, 80, 32, 300, 0, 1, StorageDataType.E4M3);
-    List<Vector> vectors = TestUtils.getTestVectors();
+    List<Vector> testVectors = TestUtils.getTestVectors();
+    try (final StringIndex index = new StringIndex(SpaceType.Cosine, testVectors.get(0).vector.length, 32, testVectors.size(), 0, testVectors.size(), StorageDataType.E4M3)) {
+      for (Vector v : testVectors) {
+        index.addItem(v.name, v.vector);
+      }
 
-    for (Vector v : vectors) {
-      index.addItem(v.uri, v.vector);
+      File tempDir = new File(TEMP_DIR_NAME);
+      if (!tempDir.mkdirs()) throw new RuntimeException("Failed to make temporary directory in test!");
+
+      try {
+        index.saveIndex(
+                Files.newOutputStream(Paths.get(TEMP_DIR_NAME, EXPECTED_INDEX_FILE_NAME)),
+                Files.newOutputStream(Paths.get(TEMP_DIR_NAME, EXPECTED_NAME_FILE_NAME)));
+        assertThat(Paths.get(TEMP_DIR_NAME, EXPECTED_INDEX_FILE_NAME).toFile()).exists();
+        assertThat(Paths.get(TEMP_DIR_NAME, EXPECTED_NAME_FILE_NAME).toFile()).exists();
+
+        final StringIndex reloadedIndex = StringIndex.load(
+                Files.newInputStream(Paths.get(TEMP_DIR_NAME, EXPECTED_INDEX_FILE_NAME)),
+                Files.newInputStream(Paths.get(TEMP_DIR_NAME, EXPECTED_NAME_FILE_NAME)),
+                SpaceType.Cosine,
+                testVectors.get(0).vector.length,
+                StorageDataType.E4M3);
+        List<CustomResult> results = RESULT_MAPPER.apply(reloadedIndex.query(TestUtils.TEST_VECTOR, 2, testVectors.size()));
+        assertThat(results)
+                .extracting(CustomResult::getName)
+                .containsExactly("my-vector-78", "my-vector-93");
+      } finally {
+        FileUtils.deleteDirectory(tempDir);
+      }
     }
-
-    File tempDir = new File("./temp");
-    tempDir.mkdirs();
-
-    index.saveIndex(
-        Files.newOutputStream(Paths.get("./temp/index.hnsw")),
-        Files.newOutputStream(Paths.get("./temp/names.json")));
-    assertThat(new File("./temp/index.hnsw")).exists();
-    assertThat(new File("./temp/names.json")).exists();
-
-    FileUtils.deleteDirectory(tempDir);
-    index.close();
   }
 
-  // @Test
-  public void itSavesToFilesystem() throws Exception {
-    StringIndex index = new StringIndex(SpaceType.Cosine, 80, 32, 300, 0, 1, StorageDataType.E4M3);
-    List<Vector> vectors = TestUtils.getTestVectors();
+  @Test
+  public void itSavesToFiles() throws Exception {
+    try (final StringIndex index = new StringIndex(SpaceType.Cosine, 80, 32, 300, 0, 1, StorageDataType.E4M3)) {
+      List<Vector> vectors = TestUtils.getTestVectors();
 
-    for (Vector v : vectors) {
-      index.addItem(v.uri, v.vector);
+      for (Vector v : vectors) {
+        index.addItem(v.name, v.vector);
+      }
+
+      File tempDir = new File(TEMP_DIR_NAME);
+      if (!tempDir.mkdirs()) throw new RuntimeException("Failed to make temporary directory in test!");
+
+      try {
+        index.saveIndex(TEMP_DIR_NAME);
+        assertThat(Paths.get(TEMP_DIR_NAME, EXPECTED_INDEX_FILE_NAME)).exists();
+        assertThat(Paths.get(TEMP_DIR_NAME, EXPECTED_NAME_FILE_NAME)).exists();
+      } finally {
+        FileUtils.deleteDirectory(tempDir);
+      }
     }
-
-    File tempDir = new File("./temp2");
-    tempDir.mkdirs();
-
-    index.saveIndex("temp2");
-
-    assertThat(new File("./temp2/index.hnsw")).exists();
-    assertThat(new File("./temp2/names.json")).exists();
-
-    FileUtils.deleteDirectory(tempDir);
-    index.close();
   }
 
-  // @Test
+  @Test
   public void itLoadsFromInputStream() throws Exception {
-    StringIndex index =
-        StringIndex.load(
-            Resources.getResource("index.hnsw").openStream(),
-            Resources.getResource("names.json").openStream(),
+    try (final StringIndex index = StringIndex.load(
+            Resources.getResource(EXPECTED_INDEX_FILE_NAME).openStream(),
+            Resources.getResource(EXPECTED_NAME_FILE_NAME).openStream(),
             SpaceType.Cosine,
             80,
-            StorageDataType.E4M3);
+            StorageDataType.E4M3)) {
 
-    List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, 2));
+      List<CustomResult> results = RESULT_MAPPER.apply(index.query(TestUtils.TEST_VECTOR, 2, 100));
 
-    assertThat(results)
-        .extracting(CustomResult::getName)
-        .containsExactly(
-            "spotify:track:0Eqm7hD828cATBLUx2fJox", "spotify:track:0Eq2AzJQNwxf5FsLYBjitC");
-
-    index.close();
-  }
-
-  // @Test
-  public void itVerifiesEfParameter() throws Exception {
-    StringIndex index =
-        StringIndex.load(
-            Resources.getResource("index.hnsw").openStream(),
-            Resources.getResource("names.json").openStream(),
-            SpaceType.Cosine,
-            80,
-            StorageDataType.E4M3);
-
-    assertThatThrownBy(() -> index.query(TestUtils.TEST_VECTOR, 2, 1))
-        .isInstanceOf(RuntimeException.class)
-        .hasMessage("queryEf must be equal to or greater than the requested number of neighbors");
+      assertThat(results)
+              .extracting(CustomResult::getName)
+              .containsExactly("my-vector-78", "my-vector-93");
+    }
   }
 
   public static class CustomResult {
@@ -198,5 +176,11 @@ public class StringIndexTest {
     public float getDistance() {
       return this.distance;
     }
+  }
+
+  private List<Float> convert(float[] input) {
+    List<Float> floats = new ArrayList<Float>(input.length);
+    for (float f : input) floats.add(f);
+    return floats;
   }
 }
