f35aef79fdac596ffa34bf2e44c8c9032bc3215e
Mark Koh <markkoh@spotify.com>
2023-11-10 11:10:09 -0700

    Fix non-deterministic query_ef test

--- python/tests/test_index_creation.py
+++ python/tests/test_index_creation.py
@@ -291,6 +291,11 @@ def test_load_incorrect_type(
 @pytest.mark.parametrize("space", [voyager.Space.Euclidean, voyager.Space.Cosine])
 @pytest.mark.parametrize("query_ef,rank_tolerance", [(1, 500), (2, 75), (100, 1)])
 def test_query_ef(space: voyager.Space, query_ef: int, rank_tolerance: int):
+    """"
+    Test to ensure that querying with random vectors will return sufficiently correct results at varying levels of
+    query_ef
+    """
+    np.random.seed(123)
     num_dimensions = 32
     num_elements = 1_000
     input_data = np.random.random((num_elements, num_dimensions)).astype(np.float32) * 2 - 1
