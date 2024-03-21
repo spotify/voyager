b7034709948370414bb82aaf681b9868de637203
Dylan Bannon <dbannon@spotify.com>
2024-03-21 17:33:53 -0400

    fmt

--- java/src/test/java/com/spotify/voyager/jni/ConcurrencyTest.java
+++ java/src/test/java/com/spotify/voyager/jni/ConcurrencyTest.java
@@ -32,7 +32,6 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ConcurrencyTest {
