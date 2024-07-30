c257e4d132e8ab4c10dae675a957cfa67fa1dcf3
Dylan Bannon <dbannon@spotify.com>
2024-07-30 12:24:12 -0400

    bump versions and regen docs

--- docs/python/_static/documentation_options.js
+++ docs/python/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.0.8',
+    VERSION: '2.0.9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- python/voyager/version.py
+++ python/voyager/version.py
@@ -17,6 +17,6 @@
 
 MAJOR = 2
 MINOR = 0
-PATCH = 8
+PATCH = 9
 
 __version__ = "%d.%d.%d" % (MAJOR, MINOR, PATCH)
