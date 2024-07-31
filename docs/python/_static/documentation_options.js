e3ce30dabec76b2ad4f4a9cf9c1e326e5544d975
Peter Sobot <psobot@spotify.com>
2024-07-31 10:21:53 -0400

    Remove negative distance check.

--- docs/python/_static/documentation_options.js
+++ docs/python/_static/documentation_options.js
@@ -1,4 +1,5 @@
-const DOCUMENTATION_OPTIONS = {
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '2.0.8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
